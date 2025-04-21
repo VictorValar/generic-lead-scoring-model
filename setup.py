4179c90a7e991eb17a991b5f0f338e8be7cb0e2f
Victor Valar <valar@victorvalar.me>
2025-04-20 23:02:20 -0300

    refactor: Transition to pipenv and Updated dependencies

--- setup.py
+++ setup.py
@@ -1,37 +1,35 @@
 from setuptools import setup, find_packages
 import codecs
 import os
+import toml
 
 here = os.path.abspath(os.path.dirname(__file__))
-requirements_filename = os.path.join(here, 'requirements.txt')
-print(requirements_filename)
+pipfile_path = os.path.join(here, 'Pipfile')
 readme_filename = os.path.join(here, 'README.md')
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+with codecs.open(readme_filename, encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-with open(requirements_filename) as f:
-    INSTALL_REQUIRES = f.read().splitlines()
+pipfile_data = toml.load(pipfile_path)
 
-# EXTRA_REQUIRES = ['pytest']
-VERSION = '0.1.3'
+packages = pipfile_data.get('packages', {})
+INSTALL_REQUIRES = [f"{pkg}{'' if spec == '*' else spec}" for pkg, spec in packages.items()]
+
+VERSION = '0.1.4'
 DESCRIPTION = 'A Python package that simplifies the process of building lead scoring models.'
-LONG_DESCRIPTION = long_description
 PACKAGE_LICENSE = 'LICENSE.txt'
 
-# Setting up
 setup(
     name="glsm",
     version=VERSION,
     license=PACKAGE_LICENSE,
     author="Victor Valar",
-    author_email="<valar@victorvalar.me>",
+    author_email="<valar@victorvalar.tech>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
-    # extras_require=EXTRA_REQUIRES,
     keywords=['python', 'lead score', 'modeling', 'lead generation', 'lead scoring', 'lead scoring model'],
     classifiers=[
         "Development Status :: 4 - Beta",
